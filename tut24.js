1255117fbcc100fa5c8f4d2a0a9805440c9d092c
Induja Kala <indujakala19@gmail.com>
2022-05-26 11:13:51 +0530

    Logs errors and warnings

---
+++ tut24.js
@@ -0,0 +1,13 @@
+console.time('Your code took');
+console.log('Hello console');  
+console.log(2+20);
+// writes same thing as a log in a console
+console.log(true);
+console.log([3,4,5,61]);
+console.log({induja: 'Name', Marks: '50'});
+console.table({induja: 'Name', Marks: '50'});
+console.warn('this is a warning');
+// console.clear();
+console.timeEnd('Your code took');
+// console.assert( 121<120,'age>120', 'This is not possible')
+console.error('this is an error');
